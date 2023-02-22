01cc9043c2bc9395ab53d5e84e9eb7401df7c451
Sretenka Doslic <sretenkad@hotmail.com>
2023-02-22 19:33:03 +0100

    Improved informing about error when missing data files

--- TaskTracker/View/FrmTaskTracker.cs
+++ TaskTracker/View/FrmTaskTracker.cs
@@ -17,12 +17,19 @@ namespace TaskTracker.View
                 {
                     dgTask.Rows.Add(new string[] { t.Name, t.Description, t.StartDate.ToString("dd/MM/yyyy"), t.DueDate.ToString("dd/MM/yyyy"),t.ResposiblePerson.Name, Type.ConvertStatusToString(t.Status) });
                 }
+            } catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            try
+            {
                 List<Person> persons = PersonDAO.GetAll();
                 foreach (Person p in persons)
                 {
-                    dgPerson.Rows.Add(new string[] { p.Name, p.Birthday.ToString("dd/MM/yyyy"), p.Email});
+                    dgPerson.Rows.Add(new string[] { p.Name, p.Birthday.ToString("dd/MM/yyyy"), p.Email });
                 }
-            } catch (FileNotFoundException ex)
+            }
+            catch (FileNotFoundException ex)
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
